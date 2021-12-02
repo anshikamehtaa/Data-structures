3516fbc2074ebaf57f690a29f88c61cb9f819b11
groot1302 <85496946+groot1302@users.noreply.github.com>
2021-12-02 18:26:46 +0530

    Add files via upload

---
+++ postorderTransversal@BT.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+struct node *create(int value)
+{
+    struct node *n;
+    n=(struct node *)malloc(sizeof(struct node));
+    n->data=value;
+    n->left=NULL;
+    n->right=NULL;
+    return n;
+}
+void postorder(struct node *p)
+{   
+    if(p!=NULL)
+    {
+        postorder(p->left);
+        postorder(p->right);
+        printf("%d ",p->data);
+        
+    }
+}
+int main()
+{
+    struct node *p=create(5);
+    struct node *p1=create(10);
+    struct node *p2=create(15);
+    struct node *p3=create(20);
+    struct node *p4=create(25);
+    
+    //Linking of nodes to form a tree
+    p->left=p1;
+    p->right=p2;
+    
+    p1->left=p3;
+    p1->right=p4;
+    
+    //Calling the preorder Transversal--
+    postorder(p);
+return 0;    
+}
+
