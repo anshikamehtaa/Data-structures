42ecb9d9abf42750f03a4365fc041c809feafafe
groot1302 <85496946+groot1302@users.noreply.github.com>
2021-12-03 19:22:18 +0530

    Add files via upload

---
+++ Forming@CLL.c
@@ -0,0 +1,48 @@
+//Circular linked list
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node *next;
+};
+int main()
+{   
+    struct node *head;
+    struct node *one;
+    struct node *two;
+    struct node *three;
+    struct node *four;
+    struct node *five;
+    
+    head=(struct node *)malloc(sizeof(struct node));
+    one=(struct node *)malloc(sizeof(struct node));
+    two=(struct node *)malloc(sizeof(struct node));
+    three=(struct node *)malloc(sizeof(struct node));
+    four=(struct node *)malloc(sizeof(struct node));
+    five=(struct node *)malloc(sizeof(struct node));
+    
+    head->data=5;
+    head->next=one;
+    
+   
+    one->data=10;
+    one->next=two;
+    
+    
+    two->data=15;
+    two->next=three;
+    
+    
+    three->data=20;
+    three->next=four;
+    
+    
+    four->data=25;
+    four->next=five;
+    
+    
+    five->data=30;
+    five->next=head;
+
+ return 0;   
+}
